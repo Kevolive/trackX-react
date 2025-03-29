1aea7ee789f89a834542b0bcf26900b1386f7886
Victo Polo <viktorpolo16@gmail.com>
2025-03-29 15:49:31 -0500

    init

---
+++ src/routes/enrutador.jsx
@@ -0,0 +1,14 @@
+import Home from '../Home'
+import Login from '../pages/Login'
+let routes =[
+    {
+        path:'/login',
+        element: <Login/>
+    },
+    {
+        path:'/home',
+        element: <Home/>
+    },
+   
+
+]
